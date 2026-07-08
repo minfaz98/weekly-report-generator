90c8d73561a57d6bea867cae9c13956d7ba908c1
minfaz <minfaz98@gmail.com>
2026-07-08 17:00:44 +0530

    Add PyMySQL module with core functionality and error handling

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -76,9 +76,13 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "weekly_report_db",
+        "USER": "root",
+        "PASSWORD": "root",
+        "HOST": "localhost",
+        "PORT": "3306",
     }
 }
 
