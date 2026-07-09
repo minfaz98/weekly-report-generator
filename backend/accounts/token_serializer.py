4d0eda791a0667c983e53524600bc3087a8706ad
minfaz <minfaz98@gmail.com>
2026-07-09 08:23:49 +0530

    feat(auth): customize JWT login response

---
+++ backend/accounts/token_serializer.py
@@ -0,0 +1,19 @@
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
+
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        return super().get_token(user)
+
+    def validate(self, attrs):
+        data = super().validate(attrs)
+
+        data["user"] = {
+            "id": self.user.id,
+            "username": self.user.username,
+            "email": self.user.email,
+            "role": self.user.role,
+        }
+
+        return data
--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,15 +1,15 @@
 from django.urls import path
 from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
     TokenRefreshView,
 )
+from .token_views import CustomTokenObtainPairView
 
 from .views import RegisterView, ProfileView
 
 urlpatterns = [
     path("register/", RegisterView.as_view(), name="register"),
 
-    path("login/", TokenObtainPairView.as_view(), name="login"),
+    path("login/",CustomTokenObtainPairView.as_view(),name="login",),
 
     path("refresh/", TokenRefreshView.as_view(), name="refresh"),
 
